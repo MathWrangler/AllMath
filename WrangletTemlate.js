aad74b63e8f99c4469ac1caf1d559453f0248a01
MathWrangler <MathWrangler@users.noreply.github.com>
2016-06-12 20:44:22 -0600

    Add files via upload
    Updated Template to have an assert function and an iMatrixDef class

--- WrangletTemlate.js
+++ WrangletTemlate.js
@@ -8,12 +8,42 @@
     <p><cite> your_wranglet_name </cite> Author: <author_name>   WrangleID: <your_id> </p>
  */
 
-function iMatrixDef() {
-    var a1, a2, a3;
-    var b1, b2, b3, b4;
-    var c1;
+// Assert if there's a problem with input range'
+function assert(condition, message) {
+    if (!condition) {
+        message = message || "Assertion failed";
+        if (typeof Error !== "undefined") {
+            throw new Error(message);
+        }
+        throw message; // Fallback
+    }
+}
 
-    var iMatrix = [[a1, a2, a3],[b1, b2, b3, b4],[c1]];
+function iMatrixDef() {  /*  iMatrixDef defines the fields' data types for the iMatrix, the field range limits and
+                              how supporting user information is to be displayed
+                         */
+    // Define the matrix dimensions with data types
+    var a1 = 0.0, a2 = 0.0, a3 = 0.0;
+    var b1 = 1, b2 = 12, b3 = 50;
+    var c1 = "yes";
+
+    // Put limits on the range of the input parameter if needed
+    assert(a1 > 100, "a1 must be less than 100");
+    assert(!(c1 == 'yes' || c1 == "Yes" || c1 == "YES" || c1 == 'no' || c1 == "No" || c1 == "NO"),
+        "c1 can only be Yes or No");
+    assert(b1 > 0 && b2 > 0 && b3 > 0,"weights must be greater than zero");
+    assert(b1 < 100 && b2 < 100 && b3 < 100,"weights must be greater than zero");
+
+    // define labels for cells
+    var a1_Label = "Vector a_x"; var a2_Label = "Vector a_y"; var a3_Label = "Vector a_z";
+    var b1_Label = "x weight"; var b2_Label = "y weight"; var b3_Label = "z weight";
+    var c1_label = "compute entire range";
+
+    //define the corresponding matrices
+    var iM = [[a1, a2, a3],[b1, b2, b3],[c1]];
+    var Labels = [[a1_Label,a2_Label, a3_Label],[b1_Label,b2_Label,b3_Label],[c1_label]];
+
+    return [iM,Labels];
 
 }
 
